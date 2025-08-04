549a97e5d3f4b0c2d7ce72191f7b24ad5815f55e
Raghavendra1032 <nraghavendra851@gmail.com>
2025-08-04 17:34:53 +0530

    code changed

--- view_all.php
+++ view_all.php
@@ -1,10 +1,11 @@
 <?php
-$host = "localhost";
+$host = "127.0.0.1";
 $user = "root";
 $password = "";
 $db = "visitor_db";
 
 $conn = new mysqli($host, $user, $password, $db);
+
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
